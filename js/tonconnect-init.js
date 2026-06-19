bb461e221bf2622814fe7e700d8a392c5dedd270
Shillare <nikitakekin25@gmail.com>
2026-06-19 20:33:05 +0300

    Add TonConnect, auction contract, place bid, bot commands

---
+++ js/tonconnect-init.js
@@ -0,0 +1,10 @@
+// TonConnect + Auction integration for Fragment Mini App
+// Uses esm.sh CDN for @tonconnect/sdk and @ton/core
+// Import happens via <script type="module"> in the HTML
+
+// The actual module code is injected inline in the template
+// to avoid CORS issues with CDN imports on static hosting.
+// This file serves as documentation and the source of truth.
+//
+// The code is embedded in template_original.html near the
+// end of the <body> as a <script type="module"> block.
